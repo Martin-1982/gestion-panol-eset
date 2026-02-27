c5684319480f9b309af04bb35c90d8879c873bf6
Martin-1982 <altamiranda.martin@gmail.com>
2026-02-27 19:45:08 -0300

    Delete db directory

--- db/migrations/003_add_rolid_to_funciones.sql
+++
@@ -1,7 +0,0 @@
--- Agrega la columna rol_id a la tabla funciones para relacionar funciones con roles
-ALTER TABLE funciones
-  ADD COLUMN IF NOT EXISTS rol_id integer;
-
--- (Opcional) crear FK si roles.id existe y se desea integridad referencial
--- ALTER TABLE funciones
---   ADD CONSTRAINT funciones_rol_id_fkey FOREIGN KEY (rol_id) REFERENCES roles(id);
