9719680c5a2781c87657e0b860c5fc44f59b39ab
Martin-1982 <altamiranda.martin@gmail.com>
2026-02-27 19:14:53 -0300

    Delete api directory

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Punto de entrada para Vercel Serverless Functions
-// Vercel llama a este archivo por cada request a /api/*
-// El archivo simplemente exporta la aplicación Express
-require('dotenv').config();
-const app = require('./src/app');
-
-module.exports = app;
