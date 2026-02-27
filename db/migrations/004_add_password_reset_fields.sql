da47d17530b9ca0160cf73ec57bc25cced707c45
Martin-1982 <altamiranda.martin@gmail.com>
2026-02-27 20:20:25 -0300

    Delete db directory

--- db/migrations/004_add_password_reset_fields.sql
+++
@@ -1,4 +0,0 @@
--- Añade campos para el restablecimiento de contraseña
-ALTER TABLE usuarios
-  ADD COLUMN IF NOT EXISTS password_reset_token_hash varchar,
-  ADD COLUMN IF NOT EXISTS password_reset_expires timestamptz;
